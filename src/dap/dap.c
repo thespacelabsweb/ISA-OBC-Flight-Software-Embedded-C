a3ebcbdfd62936a6441b5547751f7ba3aab25b94
sharu <sharususan@thespacelabs.com>
2025-08-06 11:33:37 +0530

    added dap code - correction
    Signed-off-by: sharu <sharususan@thespacelabs.com>

--- src/dap/dap.c
+++ src/dap/dap.c
@@ -30,6 +30,7 @@ Vector3 DAP_Execute(
     double deltar = ComputeDeltaRollCommand(phi, rollRate, intr, dapParams, timeStep);
     double deltap = ComputeDeltaPitchCommand(accelerationY, pitchRate, accelerationYCommand, dapParams, timeStep);
     double deltay = ComputeDeltaYawCommand(accelerationZ, yawRate, accelerationZCommand, dapParams, timeStep);
+    Vector3 output(deltar,deltap,deltay);
 }
 
 double ComputeDeltaRollCommand(double phi, double rollRate, double intr, DAPParameters dapParams, double timeStep) 
@@ -67,6 +68,7 @@ double ComputeDeltaPitchCommand(double accelerationY, double pitchRate, double a
     float accErrorPitch = accelerationYCommand - (dapParams.Kr_pitch*pitchRate)-((accelerationY+dapParams.c*qDot)*dapParams.Ka_pitch);
     previousPitchrate = currentPitchrate; /* update previous pitch rate */ 
     float deltaCommandPitch = accErrorPitch*dapParams.Ks_pitch;
+    return deltaCommandPitch;
 }
 
 double ComputeDeltaYawCommand(double accelerationZ, double yawRate, double accelerationZCommand, DAPParameters dapParams, double timeStep) 
@@ -76,4 +78,5 @@ double ComputeDeltaYawCommand(double accelerationZ, double yawRate, double accel
     float accErrorYaw = accelerationZCommand - (dapParams.Kr_yaw*yawRate)-((accelerationZ+dapParams.c*rDot)*dapParams.Ka_yaw);
     previousYawrate = currentYawrate; /* update previous yaw rate */ 
     float deltaCommandYaw = accErrorYaw*dapParams.Ks_yaw;
+    return deltaCommandYaw;
 }
